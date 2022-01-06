feb29dec9d492b5bde797c6970a4a2fddce02bed
yasikkisay <yaroslav.ig.ch@gmail.com>
2022-01-06 20:02:47 +0300

    Add list with 2 items

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,17 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         String message = "Hey boy!";
         System.out.println("Hello world! Good morning!");
         System.out.println(message);
+
+        List<String> items = new ArrayList<>();
+        items.add("banana");
+        items.add("bread");
     }
 }
