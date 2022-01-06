09332e9e79485203342fa40264d97d666c1d357b
yasikkisay <yaroslav.ig.ch@gmail.com>
2022-01-06 15:45:07 +0300

    Add one more message

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        String message = "Hey boy!";
         System.out.println("Hello world! Good morning!");
+        System.out.println(message);
     }
 }
